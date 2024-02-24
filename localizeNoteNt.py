ffdeffc546078b1ab0904b40f56657b31d3f79f0
Ademfcan <ademfcan@gmail.com>
2024-02-23 23:45:54 -0800

    updates to script

--- localizeNoteNt.py
+++ localizeNoteNt.py
@@ -39,9 +39,7 @@ def processTarget(results):
     if results != None and results[0] != None:
         boxes = results[0].boxes.xywh.cpu()
         confs = results[0].boxes.conf.cpu()
-        confidences = []
-        xs = []
-        ys = []
+        targets = []
         for box, conf in zip(boxes, confs):
             x,y,w,h = box
             
@@ -50,12 +48,11 @@ def processTarget(results):
             bearingRad = calcDefOff(HFOVRAD, HRES, diffX)
             noteRange = calculateDistance(NOTEKNOWNSIZE,w,FX)
             targetX,targetY,targetZ = turnBearingDistanceToXYZ(bearingRad,noteRange)
-            confidences.append(conf)
-            xs.append(targetX)
-            ys.append(targetY)
+            targets.append([conf,targetX,targetY,targetZ])
         
 
-    return confidences,xs,ys
+    return targets
+
 offsetsX = [14.25,-14.25,-13,13]
 offsetsZ = [13.84,13.84,-13.24,-13.24]
 rotXOffset = [80,280,188,172]
@@ -68,6 +65,8 @@ def turnBearingDistanceToXYZ(bearing, range, cameraIndex):
     print(f"dx {dx} dz {dz}")
     a = math.radians(rotXOffset[cameraIndex])
     b = math.radians(rotDownOffset[cameraIndex])
+    print(f"a {a} b {b}")
+
 
     # Define rotation matrices
     rotMatrixZ = np.array([[math.cos(a), -math.sin(a), 0],
@@ -110,25 +109,60 @@ def turnBearingDistanceToXYZ(bearing, range, cameraIndex):
     finalZ = actualFinalMatrix[2][0]
     return finalX,finalY,finalZ
 
-    
+def writeToNt(accumulatedResults):
+    confs = []
+    xs = []
+    ys = []
+    zs = []
+    for target in accumulatedResults:
+        conf = target[0] 
+        confs.append(conf)
+        x = target[1]
+        xs.append(x)
+        y = target[2]
+        ys.append(y)
+        z = target[3]
+        zs.append(z)
+    confTargets.set(confs)
+    xTargets.set(xs)
+    yTargets.set(ys)
+    zTargets.set(zs)
+
+
 
     
+# start waiting for photonvision camera updates and put on network tables
+def startLoop():
+    camera1 = PhotonCamera("testCam")
+    camera2 = PhotonCamera("testCam")
+    camera3 = PhotonCamera("testCam")
+    camera4 = PhotonCamera("testCam")
+    cams = [camera1,camera2,camera3,camera4]
+    lastTimeStamps = [-100,-100,-100,-100]
+    lastResults = [[],[],[],[]]
+    
+    
+    while True:
+        # todo get actual results
+        hasChanged = False
+        for i in range(len(cams)):
+            currCam = cams[i]
+            latestResult = currCam.getLatestResult()
+            latestTimeStamp = latestResult.getTimestamp()
+            prevTimeStamp = lastTimeStamps[i]
+            if latestTimeStamp != prevTimeStamp:
+                hasChanged = True
+                # new result
+                targets = processTarget(latestResult)
+                lastResults[i] = targets
+        
+        if hasChanged:
+            AccumulatedResults = []
+            for result in lastResults:
+                AccumulatedResults.append(result)
+            writeToNt(AccumulatedResults)
 
+                
 
-xF,yF,zF = turnBearingDistanceToXYZ(-math.pi/4,5,1)
-print(f"X: {xF} Y: {yF} Z: {zF}")
-
-
-
-
-# camera = PhotonCamera("testCam")
-# print(str(camera.getLatestResult().getTargets()))
-# # figure out how to get results
-# results = "blah blah blah"
-# confs,xs,ys,zs = processTarget(results)
-# if(confs):
-#     confTargets.set(confs)
-#     xTargets.set(xs)
-#     yTargets.set(ys)
-#     zTargets.setz(zs)
-
+if __name__ == "main":
+    startLoop()
